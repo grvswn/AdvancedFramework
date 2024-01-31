1b3fa4a60df619c41d763bd8de13a4db24682d4e
gourav <gauravswain@gmail.com>
2024-01-31 12:49:42 +0000

    added routes

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const hbs = require("hbs");
+const wax = require("wax-on");
+require("dotenv").config();
+
+let app = express();
+
+app.set("view engine", "hbs");
+app.use(express.static("public"));
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
+
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+const landingRoutes = require('./routes/landing.js');
+
+async function main() {
+    app.use('/', landingRoutes);
+}
+
+main();
+
+app.listen(3000, () => {
+  console.log("Server has started");
+});
+
