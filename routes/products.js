911ac21dc4b8bab477dd622b3ba66fe483bd2b64
gourav <gauravswain@gmail.com>
2024-02-01 13:31:20 +0000

    added crud to products

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { Product } = require('../models');
+const { createProductForm, bootstrapField } = require('../forms');
+
 const router = express.Router();
 
 router.get('/', async function(req,res){
@@ -7,6 +9,107 @@ router.get('/', async function(req,res){
     res.render("products/index",{
         products: products.toJSON()
     });
+});
+
+router.get('/create', async function(req,res){
+    const productForm = createProductForm();
+    res.render('products/create',{
+        form: productForm.toHTML(bootstrapField)
+    })
+});
+
+router.post('/create', async function(req,res){
+    const productForm = createProductForm();
+    productForm.handle(req, {
+        "success":async function(form) {
+            const product = new Product();
+            product.set('name', form.data.name);
+            product.set('cost', form.data.cost);
+            product.set('description', form.data.description);
+            await product.save();
+            res.redirect('/products');
+
+        },
+        "error": function(form) {
+            res.render('products/create',{
+                form:form.toHTML(bootstrapField)
+            })
+        },
+        "empty":function(form) {
+            res.render('products/create',{
+                form:form.toHTML(bootstrapField)
+            })
+        }
+    })
+});
+
+router.get('/:product_id/update', async function(req,res){
+
+   const product = await Product.where({
+    'id': req.params.product_id
+   }).fetch({
+    require: true 
+   }); 
+   
+   const productForm = createProductForm();
+   productForm.fields.name.value = product.get('name');
+   productForm.fields.description.value = product.get('description');
+   productForm.fields.cost.value = product.get('cost');
+
+   res.render('products/update',{
+    form: productForm.toHTML(bootstrapField)
+   });
+});
+
+router.post('/:product_id/update', async function(req,res){
+    const productForm = createProductForm();
+
+    const product = await Product.where({
+        id: req.params.product_id
+    }).fetch({
+        require: true
+    })
+
+    productForm.handle(req, {
+        "success": async function(form) {
+            product.set(form.data);
+            await product.save();
+            res.redirect('/products');
+        },
+        "error": async function(form) {
+            res.render('products/update',{
+                form: form.toHTML(bootstrapField)
+            })
+        },
+        "empty":async function(form){
+            res.render('products/update',{
+                form: form.toHTML(bootstrapField)
+            })
+        }
+    })
+});
+
+router.get('/:product_id/delete', async function(req,res){
+
+    const product = await Product.where({
+       'id': req.params.product_id
+   }).fetch({
+       require: true 
+   }); 
+
+   res.render('products/delete', {
+       product: product.toJSON()
+   })
+});
+
+router.post('/:product_id/delete', async function(req,res){
+    const product = await Product.where({
+        'id': req.params.product_id
+    }).fetch({
+        require: true 
+    }); 
+    await product.destroy();
+    res.redirect('/products');
 });
 
 module.exports = router;
